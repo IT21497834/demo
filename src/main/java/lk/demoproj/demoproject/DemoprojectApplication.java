a427bd78243f15c2276e333b7927cf3b5fcb9074
himesh nanayakkara <himeshdilankananayakkara@himeshs-MacBook-Pro.local>
2023-10-05 22:36:32 +0530

    initial commit

--- src/main/java/lk/demoproj/demoproject/DemoprojectApplication.java
+++ src/main/java/lk/demoproj/demoproject/DemoprojectApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoprojectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoprojectApplication.class, args);
+		
 	}
 
 }
